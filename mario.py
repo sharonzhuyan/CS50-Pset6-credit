07992dd408ec0066fb8d7c4bf38e740f2f77486f
sharonzhuyan <sharonzhuyan@users.noreply.github.com>
2019-12-22 15:49:53 +0000

    automated commit by submit50

--- mario.py
+++ mario.py
@@ -1,9 +1,10 @@
 from cs50 import get_int
+# prompt user for correct imput
 while True:
     x = get_int("Height: ")
-    if x >= 1 and x<=8:
+    if x >= 1 and x <= 8:
         i = 1
         while i <= x:
             print(" " * (x-i) + "#"*i + "  " + "#"*i)
-            i+=1
+            i += 1
         break
--- trial.py
+++
@@ -1,4 +0,0 @@
-from cs50 import get_int
-while True:
-    x = get_int('x=')
-    print(x)
