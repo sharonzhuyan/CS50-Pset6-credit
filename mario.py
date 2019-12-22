3342f53eb93e33e643d82aa98836c877e2d08894
sharonzhuyan <sharonzhuyan@users.noreply.github.com>
2019-12-22 15:36:58 +0000

    automated commit by check50 [check50=True]

---
+++ mario.py
@@ -0,0 +1,11 @@
+from cs50 import get_int
+while True:
+    x = get_int("Height: ")
+    if x >= 1 and x<=8:
+        i = 1
+        while i <= x:
+            print(" " * (x-i) + "#"*i + "  " + "#"*i)
+            i+=1
+        break
+    else:
+        break
